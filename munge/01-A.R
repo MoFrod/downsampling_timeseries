a9b2da6e3dfc553a9800bdc530b21dcf89a69538
MoFrod <morgan@frodsham.net>
2023-06-26 19:39:14 +0100

    run downsampled data through imputeTS

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,25 +1,38 @@
-# Create data frames from JSON files of Turing Change Point Dataset (TCPD).
-                        
-# Bee data
-bee_df <- fromJSON(file='D:/Morgan/Documents/NCL MSc/final_project/mofrod_project/data/bee_waggle_6.json')
-bee_df <- as.data.frame(ozone_df)
+# Create data frames from JSON files of Turing Change Annotated Change Dataset.
 
-# Bitcoin data
-bitcoin_df <- fromJSON(file='D:/Morgan/Documents/NCL MSc/final_project/mofrod_project/data/bitcoin.json')
-bitcoin_df <- as.data.frame(bitcoin_df)
+# Set base path used for all file loading.
+BASE_PATH = "~/Documents/MF MSc/downsampling_timeseries/data/AnnotateChange"
 
-# Children per women data
-children_per_woman_df <- fromJSON(file='D:/Morgan/Documents/NCL MSc/final_project/mofrod_project/data/children_per_woman.json')
-children_per_woman_df <- as.data.frame(children_per_woman_df)
+# Load JSON files. These can be obtained from 
+f100 <- jsonlite::fromJSON(here(BASE_PATH,"demo_100.json"))
+f200 <- jsonlite::fromJSON(here(BASE_PATH,"demo_200.json"))
+f300 <- jsonlite::fromJSON(here(BASE_PATH,"demo_300.json"))
+f400 <- jsonlite::fromJSON(here(BASE_PATH,"demo_400.json"))
+f500 <- jsonlite::fromJSON(here(BASE_PATH,"demo_500.json"))
+f600 <- jsonlite::fromJSON(here(BASE_PATH,"demo_600.json"))
+f700 <- jsonlite::fromJSON(here(BASE_PATH,"demo_700.json"))
+f800 <- jsonlite::fromJSON(here(BASE_PATH,"demo_800.json"))
 
-# Measles data
-measles_df <- fromJSON(file='D:/Morgan/Documents/NCL MSc/final_project/mofrod_project/data/measles.json')
-measles_df <- as.data.frame(measles_df)
+# Extract values, add an index column, and a column to identify each dataset.
+df100 <- data.frame(val = f100$series$raw[[1]]) %>% mutate(id = row_number(), dataset = "100")
+df200 <- data.frame(val = f200$series$raw[[1]]) %>% mutate(id = row_number(), dataset = "200")
+df300 <- data.frame(val = f300$series$raw[[1]]) %>% mutate(id = row_number(), dataset = "300")
+df400 <- data.frame(val = f400$series$raw[[1]]) %>% mutate(id = row_number(), dataset = "400")
+df500 <- data.frame(val = f500$series$raw[[1]]) %>% mutate(id = row_number(), dataset = "500")
+df600 <- data.frame(val = f600$series$raw[[1]]) %>% mutate(id = row_number(), dataset = "600")
+df700 <- data.frame(val = f700$series$raw[[1]]) %>% mutate(id = row_number(), dataset = "700")
+df800a <- data.frame(val = f800$series$raw[[1]]) %>% mutate(id = row_number(), dataset = "800A")
+df800b <- data.frame(val = f800$series$raw[[2]]) %>% mutate(id = row_number(), dataset = "800B")
 
-# Ozone data
-ozone_df <- fromJSON(file='D:/Morgan/Documents/NCL MSc/final_project/mofrod_project/data/ozone.json')
-ozone_df <- as.data.frame(ozone_df)
+# Airgap sample data
+airgap <- data.frame(val=as.matrix(tsAirgap)#, id=time(tsAirgap)
+) %>% mutate(id = row_number(), dataset = "airgap")
 
-# Rail data
-rail_lines_df <- fromJSON(file='D:/Morgan/Documents/NCL MSc/final_project/mofrod_project/data/rail_lines.json')
-rail_lines_df <- as.data.frame(rail_lines_df)
+# Merge all data sets into a single dataframe.
+merged <- rbind(df100, df200, df300, df400, df500, df600, df700, df800a, df800b, airgap)
+
+# Simple visualisation of each dataset, one per facet.
+tsp <- merged %>% ggplot(aes(x = id, y = val)) + geom_line() + geom_point() + facet_grid(dataset~., scales = "free")
+
+# Show the visualisation
+tsp
