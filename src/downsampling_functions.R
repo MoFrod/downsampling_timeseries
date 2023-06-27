eab968659dfed045a389054283fd96a3381a868b
MoFrod <morgan@frodsham.net>
2023-06-27 20:19:55 +0100

    eda downsampling visualisations with imputeTS and heatmaps

---
+++ src/downsampling_functions.R
@@ -0,0 +1,31 @@
+# Jettison Code MVP: Downsampling Functions
+
+# Simple decimation a.k.a. "every n"
+funSimpleDecimation <- function(df, i = 1) {
+  df %>% slice(seq(1, nrow(df), by = i))
+}
+
+# Percentage change based on previous value (irrespective whether it was transmitted).
+funPercentageChangeLastValue <- function(df, i = 1) {
+  df %>% mutate(perc = 100 * (val - lag(val))/lag(val)) %>% filter(abs(perc) > i)
+}
+
+# Percentage change based on last transmitted value.
+funPercentageChange <- function(df, i = 1)
+{
+  latestval = df[1,]['val']
+  id_to_keep <- c(1)
+  
+  for (j in 2:nrow(df))
+  {
+    recentval <- df[j,]['val'] %>% pull()
+    perc = 100* (recentval - latestval)/latestval
+    if (abs(perc) > i) {
+      id_to_keep <- append(id_to_keep,j)
+      latestval <- df[j,]['val']
+    } 
+  }
+  df[id_to_keep,]
+}
+
+
---
+++ src/downsampling_imputeTS_visualisations_df100.R
@@ -0,0 +1,147 @@
+# Expand the ts values into rows
+expand_ts <- df %>% separate_rows(full_ts) # %>% select(param, dataset, full_ts) %>%
+
+## Filter expand_ts for df100 by method and param
+## ==
+
+# Param 1: Simple Decimation, df100
+sd_100_1 <- expand_ts %>% filter(method == "simpleDecimation",
+                          dataset == "df100",
+                          param == "1") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 1: Simple Decimation, df100 with imputeTS
+ggplot_na_distribution(sd_100_1 %>% select(full_ts))
+
+
+# Param 2: Simple Decimation, df100
+sd_100_2 <- expand_ts %>% filter(method == "simpleDecimation",
+                            dataset == "df100",
+                            param == "2") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 2: Simple Decimation, df100 with imputeTS
+ggplot_na_distribution(sd_100_2 %>% select(full_ts))
+
+# Param 5: Simple Decimation, df100
+sd_100_5 <- expand_ts %>% filter(method == "simpleDecimation",
+                            dataset == "df100",
+                            param == "5") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 5: Simple Decimation, df100 with imputeTS
+ggplot_na_distribution(sd_100_5 %>% select(full_ts))
+
+# Param 10: Simple Decimation, df100
+sd_100_10 <- expand_ts %>% filter(method == "simpleDecimation",
+                            dataset == "df100",
+                            param == "10") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 10: Simple Decimation, df100 with imputeTS
+ggplot_na_distribution(sd_100_10 %>% select(full_ts))
+
+# Param 20: Simple Decimation, df100
+sd_100_20 <- expand_ts %>% filter(method == "simpleDecimation",
+                                  dataset == "df100",
+                                  param == "20") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 20: Simple Decimation, df100 with imputeTS
+ggplot_na_distribution(sd_100_20 %>% select(full_ts))
+
+## ==
+
+# Param 1: Percentage Change, df100
+pc_100_1 <- expand_ts %>% filter(method == "percentageChange",
+                                 dataset == "df100",
+                                 param == "1") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 1: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_1 %>% select(full_ts))
+
+
+# Param 2: Percentage Change, df100
+pc_100_2 <- expand_ts %>% filter(method == "percentageChange",
+                                 dataset == "df100",
+                                 param == "2") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 2: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_2 %>% select(full_ts))
+
+# Param 5: Percentage Change, df100
+pc_100_5 <- expand_ts %>% filter(method == "percentageChange",
+                                 dataset == "df100",
+                                 param == "5") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 5: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_5 %>% select(full_ts))
+
+# Param 10: Percentage Change, df100
+pc_100_10 <- expand_ts %>% filter(method == "percentageChange",
+                                  dataset == "df100",
+                                  param == "10") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 10: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_10 %>% select(full_ts))
+
+# Param 20: Percentage Change, df100
+pc_100_20 <- expand_ts %>% filter(method == "percentageChange",
+                                  dataset == "df100",
+                                  param == "20") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 20: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_20 %>% select(full_ts))
+
+## ==
+
+# Param 1: Percentage Change, df100
+pc_100_1 <- expand_ts %>% filter(method == "percentageChange",
+                                 dataset == "df100",
+                                 param == "1") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 1: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_1 %>% select(full_ts))
+
+
+# Param 2: Percentage Change, df100
+pc_100_2 <- expand_ts %>% filter(method == "percentageChange",
+                                 dataset == "df100",
+                                 param == "2") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 2: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_2 %>% select(full_ts))
+
+# Param 5: Percentage Change, df100
+pc_100_5 <- expand_ts %>% filter(method == "percentageChange",
+                                 dataset == "df100",
+                                 param == "5") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 5: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_5 %>% select(full_ts))
+
+# Param 10: Percentage Change, df100
+pc_100_10 <- expand_ts %>% filter(method == "percentageChange",
+                                  dataset == "df100",
+                                  param == "10") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 10: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_10 %>% select(full_ts))
+
+# Param 20: Percentage Change, df100
+pc_100_20 <- expand_ts %>% filter(method == "percentageChange",
+                                  dataset == "df100",
+                                  param == "20") %>% 
+  mutate(full_ts = as.numeric(full_ts))
+
+# Visualise Param 20: Percentage Change, df100 with imputeTS
+ggplot_na_distribution(pc_100_20 %>% select(full_ts))
